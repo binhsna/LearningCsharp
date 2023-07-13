14991ccd2366a98d06eebe951f250f0d4d10a020
binhsna <99773978+binhsna@users.noreply.github.com>
2023-07-13 15:02:58 +0700

    Init
    Init

---
+++ Console/UsePackage/UsePackage/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Text;
+using BNC.Utils;
+
+namespace UsePackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // fix xuất ra màn hình bằng tiếng việt
+            Console.OutputEncoding = Encoding.Unicode;
+            // fix nhập vào bằng tiếng việt
+            Console.InputEncoding = Encoding.Unicode;
+
+            const string lbName = "Nhập tên: ";
+            const string lbShowName = "Xin chào: ";
+            Console.Write(lbName);
+            var name = Console.ReadLine();
+            Console.WriteLine($"{lbShowName}{name}");
+            double a = 438932843;
+            var result = ConvertMoneyToText.NumberToText(a);
+            Console.WriteLine($"Số tiền trong tài khoản của bạn: {result}");
+        }
+    }
+}
