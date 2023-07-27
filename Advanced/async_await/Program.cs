c342f6636418db1346e6aa9a3357276afff9cae6
binhsna <99773978+binhsna@users.noreply.github.com>
2023-07-27 23:21:33 +0700

    Type

---
+++ Advanced/async_await/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace async_await
+{
+    class Program
+    {
+        static void DoSomeThing(int seconds, string mgs, ConsoleColor color)
+        {
+            lock (Console.Out)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine($"{mgs,10} ... Start");
+                Console.ResetColor();
+            }
+            string a = "abc";
+            //...
+            lock (a)
+            {
+                //...
+            }
+
+            for (int i = 1; i <= seconds; i++)
+            {
+                lock (Console.Out)
+                {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine($"{mgs,10} {i,2}");
+                    Thread.Sleep(1000);
+                    Console.ResetColor();
+                }
+            }
+            lock (Console.Out)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine($"{mgs,10} ... End");
+                Console.ResetColor();
+            }
+        }
+
+        // asynchronous (Đa luồng: Multi thread) -> Bất đồng bộ
+        static async Task Task2()
+        {
+            Task t2 = new Task(() =>
+                       {
+                           DoSomeThing(10, "T2", ConsoleColor.Green);
+                       }); // () => {}
+            t2.Start();
+            await t2; // Trả về task luôn -> Không cần return
+            Console.WriteLine("T2 da hoan thanh");
+        }
+        static async Task Task3()
+        {
+            Task t3 = new Task((object obj) =>
+                 {
+                     string tentacvu = (string)obj;
+                     DoSomeThing(10, tentacvu, ConsoleColor.Yellow);
+                 }, "T3"); // (Object ob) => {}
+            t3.Start();
+            await t3; // Trả về task luôn -> Không cần return
+            Console.WriteLine("T3 da hoan thanh");
+        }
+
+        // async/await
+        static async Task Abc()
+        {
+            // Task task = new Task(() =>
+            // {
+            //     // ... Các chỉ thị
+            // });
+            // task.Start();
+            // await task;
+            // ...
+            await File.WriteAllTextAsync("1.txt", "Nội dung");
+            // ...
+        }
+        static async Task<string> Task4()
+        {
+            Task<string> t4 = new Task<string>(
+                      () =>
+                      {
+                          DoSomeThing(10, "T4", ConsoleColor.Green);
+                          return "Return from T4";
+                      }
+                  ); // () => {return string;} 
+            t4.Start();
+            var kq = await t4;
+            Console.WriteLine("T4 hoan thanh");
+            return kq;
+        }
+        static async Task<string> Task5()
+        {
+            Task<string> t5 = new Task<string>(
+               (object obj) =>
+               {
+                   string t = (string)obj;
+                   DoSomeThing(4, t, ConsoleColor.Yellow);
+                   return $"Return from {t}";
+               }, "T5"); // (object obj) => {return string;} 
+
+            t5.Start();
+            string kq = await t5;
+            Console.WriteLine("T5 hoan thanh");
+            return kq;
+        }
+        static async Task<string> GetWeb(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            Console.WriteLine("Bat dau tai");
+            HttpResponseMessage kq = await httpClient.GetAsync(url);
+            Console.WriteLine("Bat dau doc noi dung");
+            string content = await kq.Content.ReadAsStringAsync();
+            Console.WriteLine("Hoan thanh");
+            return content;
+        }
+        static async Task Main(string[] args)
+        {
+            // synchronous (Đơn luồng) -> Đồng bộ
+
+            // DoSomeThing(10, "T2", ConsoleColor.Green);
+            // DoSomeThing(4, "T3", ConsoleColor.Yellow);
+
+            // asynchronous (Đa luồng): Chạy song song với nhau -> Không đồng bộ
+            // Task
+            // Task t2 = new Task(Action);// () => {}
+            // Task t3 = new Task(Action<Object>, Object);// (Object obj) => {}
+
+            // Task t2 = Task2(); // Thread
+            // Task t3 = Task3(); // Thread
+            // DoSomeThing(6, "T1", ConsoleColor.Magenta); // Thread
+
+            // // Wait() -> Phải đợi task kết thúc mới chạy tiếp code
+            // // t2.Wait();
+            // // t3.Wait();
+            // // Task.WaitAll(t2, t3);
+            // await t2;
+            // await t3;
+
+            // Task<T> -> Khi tak chạy xong -> Có giá trị trả về
+            // Task<string> t4 = new Task<string>(Func<string>); // () => {return string;} 
+            // Task<string> t4 = new Task<string>(Func<object, string>, object); // (object obj) => {return string;} 
+            Task<string> t4 = Task4();
+            Task<string> t5 = Task5();
+
+            DoSomeThing(6, "T1", ConsoleColor.Magenta);
+
+            var kq4 = await t4;
+            var kq5 = await t5;
+
+            Console.WriteLine(kq4);
+            Console.WriteLine(kq5);
+
+            //
+            var task = GetWeb("https://xuanthulab.net");
+            DoSomeThing(6, "T1", ConsoleColor.Magenta);
+
+            var content = await task;
+            Console.WriteLine(content);
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Advanced/type_attr/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace type_attr
+{
+    class Program
+    {
+        // Type -> Là lớp Chứa những thông tin về một kiểu dữ liệu nào đó: class, struct, ... int, bool ...
+        // Attribute: Thuộc tính bổ sung -> Sử dụng trong các thư viện là chính
+        // Kỹ thuật Reflection: Thông tin kiểu dữ liệu ở thời điểm thực thi
+        // AttributeName
+
+        // ObsoleteAttribute
+
+        /**
+            Mô tả (Mota)
+                - Thông tin chi tiết (Thongtinchitiet)
+        */
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)] // Mota được sử dụng ở đâu (lớp, property, method,...)
+        class MotaAttribute : Attribute
+        {
+            public string Thongtinchitiet { get; set; }
+            public MotaAttribute(string _Thongtinchitiet)
+            {
+                Thongtinchitiet = _Thongtinchitiet;
+            }
+
+        }
+        [Mota("Lớp chứa thông tin về User trên hệ thống")]
+        class User
+        {
+            [Mota("Tên người dùng")]
+            [Required(ErrorMessage = "Name phải được thiết lập")]
+            [StringLength(100, MinimumLength = 3, ErrorMessage = "Tên phải dài từ 3 đến 100 ký tự")]
+            public string Name { get; set; }
+            [Mota("Dữ liệu tuổi")]
+            [Range(18, 80, ErrorMessage = "Tuổi phải từ 18 - 80")]
+            public int Age { get; set; }
+            [Mota("Số điện thoại cá nhân")]
+            [Phone(ErrorMessage = "Số điện thoại phải đúng định dạng")]
+            public string PhoneNumber { get; set; }
+            [Mota("Địa chỉ Email")]
+            [EmailAddress(ErrorMessage = "Địa chỉ email sai cấu trúc")]
+            public string Email { get; set; }
+            // Đánh dấu là lỗi thời (Obsolete)
+            [Obsolete("Phương thức này không nên sử dụng nữa, cần thay bởi ShowInfo")]
+            public void PrintInfo() => Console.WriteLine(Name);
+        }
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            int[] a = { 1, 2, 3 };
+
+            Type t1 = typeof(int);
+            var t2 = typeof(string);
+            var t3 = typeof(Array);
+            var t = a.GetType();
+            Console.WriteLine(t);
+            Console.WriteLine("------------ Các thuộc tính");
+            t.GetProperties().ToList().ForEach(
+                (PropertyInfo o) =>
+                {
+                    Console.WriteLine(o.Name);
+                });
+            Console.WriteLine("------------ Các phương thức");
+            t.GetMethods().ToList().ForEach(
+                (MethodInfo o) =>
+                {
+                    Console.WriteLine(o.Name);
+                });
+            //
+            User user = new User()
+            {
+                Name = "BinhNC",
+                Age = 21,
+                PhoneNumber = "0971912776",
+                Email = "binhNC@gmail.com"
+            };
+            var properties = user.GetType().GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                foreach (var attr in property.GetCustomAttributes(false))
+                {
+                    MotaAttribute mota = attr as MotaAttribute;
+                    if (mota != null)
+                    {
+                        Console.WriteLine(mota.Thongtinchitiet);
+                        var value = property.GetValue(user);
+                        var name = property.Name;
+                        Console.WriteLine($"({name}) - {mota.Thongtinchitiet} : {value}");
+                    }
+                }
+            }
+            user.PrintInfo();
+
+            User user1 = new User()
+            {
+                Name = "ab",
+                Age = 21,
+                PhoneNumber = "0971912776",
+                Email = "binhNC@gmail.com"
+            };
+            ValidationContext context = new ValidationContext(user1);
+            var result = new List<ValidationResult>();
+            bool kq = Validator.TryValidateObject(user1, context, result, true);
+            if (kq == false)
+            {
+                result.ToList().ForEach(err =>
+                {
+                    Console.WriteLine(err.MemberNames.First());
+                    Console.WriteLine(err.ErrorMessage);
+                });
+            }
+
+        }
+    }
+}
