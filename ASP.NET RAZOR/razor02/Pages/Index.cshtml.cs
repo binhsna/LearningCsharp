ff96de25fae309a5e678cdf430c804193640d28a
binhsna <99773978+binhsna@users.noreply.github.com>
2023-08-10 22:03:32 +0700

    Cú pháp trong Razor Page

---
+++ ASP.NET RAZOR/razor02/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace razor02.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Abc { set; get; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            this.Abc = "Đây là nội dung trong Abc";
+        }
+    }
+}
