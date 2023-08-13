e7084a0c6b785e1b03599c2acf6626fbbc8e3f0c
binhsna <99773978+binhsna@users.noreply.github.com>
2023-08-13 11:23:58 +0700

    TagHelper và HtmlHelper

---
+++ ASP.NET RAZOR/tagHelper/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace tagHelper.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [DisplayName("Tên người dùng")]
+        public string UserName { get; set; } = "BinhNC";
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
